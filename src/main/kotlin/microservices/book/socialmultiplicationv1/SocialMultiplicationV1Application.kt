49903c366208cd9b00488cb1eb0dd619731df8dc
olakra <olakra@users.noreply.github.com>
2018-10-07 09:11:19 -0400

    Adding initial project files generated by the start.spring.io project generator

---
+++ src/main/kotlin/microservices/book/socialmultiplicationv1/SocialMultiplicationV1Application.kt
@@ -0,0 +1,11 @@
+package microservices.book.socialmultiplicationv1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialMultiplicationV1Application
+
+fun main(args: Array<String>) {
+    runApplication<SocialMultiplicationV1Application>(*args)
+}
